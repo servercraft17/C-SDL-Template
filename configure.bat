3303251aeadd4c4d2bb46336f987b81dcd0281a3
servercraft17 <92604020+servercraft17@users.noreply.github.com>
2025-11-30 12:17:55 -0500

    Enhance configure.bat to download STB and GLM
    Added commands to download STB header files and GLM library.

--- configure.bat
+++ configure.bat
@@ -8,8 +8,24 @@ del SDL.zip
 
 cd ..
 
+@REM Download STB header files.
 mkdir libs\stb
 curl -Lo libs/stb/stb_image.h https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h
 curl -Lo libs/stb/stb_image_resize2.h https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image_resize2.h
+
+@REM Download GLM and prepare it.
+mkdir libs\cglm
+curl -Lo libs/cglm.zip https://github.com/recp/cglm/archive/refs/tags/v0.9.6.zip
+cd libs && tar -xzf cglm.zip -C cglm --strip-components=1
+del cglm.zip
+
+cmake -Scglm -Bcglm/build -G"MinGW Makefiles" -DCMAKE_MAKE_PROGRAM=make
+cd cglm/build && make
+cd ../..
+
+cd ..
+
+copy libs\cglm\build\libcglm.dll.a build
+copy libs\cglm\build\libcglm-0.dll build
 
 copy libs\SDL\x86_64-w64-mingw32\bin\SDL3.dll build
